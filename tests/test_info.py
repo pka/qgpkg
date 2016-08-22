3998db4380311f4aa06be36a4ee3abf1c47b5791
Pirmin Kalberer <pka@sourcepole.ch>
2016-08-22 18:15:06 +0200

    Test dataset

--- tests/test_info.py
+++ tests/test_info.py
@@ -2,6 +2,17 @@ import sys
 from qgisgpkg.qgpkg import QGpkg
 
 
+def test_info():
+    gpkg = QGpkg('tests/data/small_world.gpkg')
+    gpkg.info()
+    output = sys.stdout.getvalue().strip()
+    info = """gpkg_contents features:
+ne_110m_admin_0_countries
+gpkg_contents tiles:
+small_world"""
+    assert output == info
+
+
 def test_wrong_file():
     gpkg = QGpkg('wrong_file_name.gpkg')
     gpkg.info()
